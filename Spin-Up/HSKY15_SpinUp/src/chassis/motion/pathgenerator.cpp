29e294859cc1c7dda25233e25af8d15ae27932e2
Sharwin Patil <sharwin24@gmail.com>
2022-11-02 16:29:03 -0700

    Injection pseudo

--- Spin-Up/HSKY15_SpinUp/src/chassis/motion/pathgenerator.cpp
+++ Spin-Up/HSKY15_SpinUp/src/chassis/motion/pathgenerator.cpp
@@ -12,13 +12,32 @@ PathGenerator::PathGenerator(MotionConstraints constraints, float deltaTime) : c
 PathGenerator::~PathGenerator() {}
 
 // TODO: Implement PathGeneration from Pose waypoints and ControlVector waypoints via waypoint injection/smoothing
-Path PathGenerator::generatePath(std::vector<RobotPose> waypoints) { return Path(); }
-Path PathGenerator::generatePath(std::vector<ControlVector> waypoints) { return Path(); }
+Path PathGenerator::generatePath(std::vector<RobotPose> waypoints) {
+    return Path();
+}
+Path PathGenerator::generatePath(std::vector<ControlVector> waypoints) {
+    return Path();
+}
 
-std::vector<RobotPose> PathGenerator::injectWaypoints(std::vector<RobotPose> waypoints) { return std::vector<RobotPose>(); }
-std::vector<ControlVector> PathGenerator::injectWaypoints(std::vector<ControlVector> waypoints) { return std::vector<ControlVector>(); }
+std::vector<RobotPose> PathGenerator::injectWaypoints(std::vector<RobotPose> waypoints) {
+    // Spacing = Desired distance between waypoints
+    // Find all line segments that compose the given set of waypoints -> (StartPosition, EndPosition)
+    // For every line segment:
+    // Define a vector as the endPoint - startPoint
+    // Find the number of waypoints to be injected -> Math.ceil(vector.magnitude / spacing)
+    // Normalize the vector -> vector = vector.normalize() * spacing
+    // for i = 0 to numPointsToBeInjected: Add startPoint + (vector * i) to path
+    // After all line segments, add last point to path
+}
+std::vector<ControlVector> PathGenerator::injectWaypoints(std::vector<ControlVector> waypoints) {
+    return std::vector<ControlVector>();
+}
 
-std::vector<RobotPose> PathGenerator::smoothWaypoints(std::vector<RobotPose> waypoints) { return std::vector<RobotPose>(); }
-std::vector<ControlVector> PathGenerator::smoothWaypoints(std::vector<ControlVector> waypoints) { return std::vector<ControlVector>(); }
+std::vector<RobotPose> PathGenerator::smoothWaypoints(std::vector<RobotPose> waypoints) {
+    return std::vector<RobotPose>();
+}
+std::vector<ControlVector> PathGenerator::smoothWaypoints(std::vector<ControlVector> waypoints) {
+    return std::vector<ControlVector>();
+}
 
 } // namespace src::Motion
