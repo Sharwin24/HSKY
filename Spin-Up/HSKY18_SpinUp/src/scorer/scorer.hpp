e0548ff360c2c90d1fb9e2c5b2d3932679ba67fa
Sharwin Patil <sharwin24@gmail.com>
2022-11-14 17:54:46 -0800

    removed flywheel encoder

--- Spin-Up/HSKY18_SpinUp/src/scorer/scorer.hpp
+++ Spin-Up/HSKY18_SpinUp/src/scorer/scorer.hpp
@@ -74,6 +74,5 @@ static MotorGroup flywheelMotorGroup = {
 
 // Scorer sensors
 static pros::Optical opticalSensor = pros::Optical(OPTICAL_SENSOR_PORT);
-static pros::Rotation flywheelEncoder = pros::Rotation(FLYWHEEL_ENCODER_PORT);
 
 } // namespace src::Scorer
